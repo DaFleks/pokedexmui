d70dac922d0970ef602920a36bc0af9f2dfc0974
DaFleks <petropoulosalex@gmail.com>
2022-11-25 20:20:15 -0500

    temporary id routing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
   const id = useLocation().pathname.replace('/', '');
+
+  const getId = (id) => id > 0 ? id : 1;
+
+
   return (
     <div className="App">
       <Routes>
-        <Route path="*" element={<Pokedex id={id} />}></Route>
+        <Route path="*" element={<Pokedex id={getId(id)} />}></Route>
       </Routes>
     </div>
   );
